796bdfcdd44095dd15372ea97d7f8ec35e87bfc5
daiwk <daiwk1989@gmail.com>
2025-09-24 16:16:29 +0800

    fix

--- sync.sh
+++ sync.sh
@@ -1,9 +1,11 @@
-cp ../collections/pdfs/llm_recommend.pdf .
+cp ../pdfs_save/llm_recommend.pdf .
 cp ../collections/posts/llm_recommend.md.raw .
 grep "^#" ./llm_recommend.md.raw | grep -v "###" | python3 replace.py > ./llm_recommend.md
 cat readme.pre llm_recommend.md > README.md
 
 rm ./llm_recommend.md.raw llm_recommend.md
+rm ./llm_recommend.pdf
+rm 
 git add .
 git commit -m 'fix'
 git push

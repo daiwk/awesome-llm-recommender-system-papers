168a114398b492a6326ab8d0be4cd1ae2fcbeaa1
daiwk <daiwk1989@gmail.com>
2025-09-24 19:50:19 +0800

    fix

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-cp ../pdfs_save/llm_recommend.pdf .
+cp ~/Downloads/同步空间/daiwk_llm_pdfs/llm_recommend.pdf .
 cp ../collections/posts/llm_recommend.md.raw .
 grep "^#" ./llm_recommend.md.raw | grep -v "###" | python3 replace.py > ./llm_recommend.md
 cat readme.pre llm_recommend.md > README.md
